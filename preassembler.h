338b6a2d517a1fd7c4756d23fb4e7322e31c7172
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-07-06 16:42:54 +0300

    Update preassembler.h

--- preassembler.h
+++ preassembler.h
@@ -4,7 +4,11 @@
 #define UNTITLED4_PREASSEMBLER_H
 
 
-
+/*
+This function reads the content of macro(asm file) file and inserts it to a linked list
+@param none
+@return none
+*/
 void file_handler();
 
 #endif 
