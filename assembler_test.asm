62835d3bf48c40bb23eb246f781e3364bc6a4730
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-07-06 16:29:01 +0300

    Add files via upload

---
+++ assembler_test.asm
@@ -0,0 +1,18 @@
+MAIN:    mov    S1.1 ,LENGTH
+         add    r2,STR
+LOOP:    jmp    END
+	 macro m1
+	 	inc 	K
+	 	
+	 	mov	S1.2,r3
+	 endmacro
+         prn    #-5
+         sub    r1, r4
+         m1
+         bne    LOOP
+END:     hlt
+STR:     .string "abcdef"
+LENGTH:  .data  6,-9,15
+K:       .data  22
+S1:      .struct 8, "ab"
+
---
+++ preassembler.h
@@ -0,0 +1,10 @@
+
+
+#ifndef UNTITLED4_PREASSEMBLER_H
+#define UNTITLED4_PREASSEMBLER_H
+
+
+
+void file_handler();
+
+#endif 
