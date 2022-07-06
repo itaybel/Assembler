24be6933e7ec0fd56b7031eb70b56b8003478cc2
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-07-06 17:20:05 +0300

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -23,6 +23,39 @@ linklist_t CreatLinkList(){
 }
 
 
+void InserNode(struct Node** head_ref,int new_data)
+    {
+        struct Node* new_node =
+                (struct Node*) malloc(sizeof(struct Node));
+
+        struct Node *last = *head_ref;
+
+        /*Put in the data*/
+        new_node->data  = new_data;
+
+        /*3. This new node is going to be the*/
+        /*last node, so make next of it as NULL*/
+        new_node->next = NULL;
+
+        /*4. If the Linked List is empty, then make*/
+        /*the new node as head*/
+        if (*head_ref == NULL)
+        {
+            *head_ref = new_node;
+            return;
+        }
+
+        /*5. Else traverse till the last node*/
+        while (last->next != NULL) {
+            last = last->next;
+
+            /* 6. Change the next of last node*/
+            last->next = new_node;
+        }
+   }
+
+
+
 struct Node *SearchNode(linklist_t head, void *data, int(*cmp_func)(void*, void*)){
     while(head!= NULL)
     {
