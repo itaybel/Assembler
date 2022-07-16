c6c1bbcbe14a76faeb24883949fbc9b2dd72b921
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-07-16 16:13:48 +0300

    Update RegisterTable.h

--- RegisterTable.h
+++ RegisterTable.h
@@ -2,4 +2,20 @@
 #define UNTITLED4_REGISTERTABLE_H
 #define NUM_OF_REGISTERS 7
 
+/*
+This function is checking if the received name of the registerTable is valid or not.
+@param name - tha name of the registerTable
+@return char name - name of the registerTable
+*/
+char* getRegisterName(char *name);
+
+
+
+/*
+This function receives the RegisterNumber and if the name of the register is valid it returns the registerNumber of the valid name.
+@param name - tha name of the registerTable
+@return int of the registerTable
+*/
+int getRegisterNum(char *name);
+
 #endif
