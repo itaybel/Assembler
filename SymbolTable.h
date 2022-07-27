934e4aa29e680499ba6a78c01f08e51e6f37689e
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-07-27 16:18:58 +0300

    Update SymbolTable.h

--- SymbolTable.h
+++ SymbolTable.h
@@ -1,12 +1,10 @@
 
-
 #ifndef UNTITLED4_SYMBOLTABLE_H
 #define UNTITLED4_SYMBOLTABLE_H
 #include "LinkedList.h"
 #define MAX_LENGTH 30
 
-
-typedef struct  SymbolNode;
+int validLabelName(char *name);
 
 int compareSymbol(void* macro, void* name);
 struct SymbolNode* createSymbolNode(char* name);
