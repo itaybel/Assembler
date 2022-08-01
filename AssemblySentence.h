1cd4126f3f46c1959e26336af9e200d1a6a729b1
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-08-01 18:20:15 +0300

    Update AssemblySentence.h

--- AssemblySentence.h
+++ AssemblySentence.h
@@ -2,17 +2,38 @@
 
 #ifndef UNTITLED4_ASSEMBLYSENTENCE_H
 #define UNTITLED4_ASSEMBLYSENTENCE_H
+
+#include "SymbolTable.h"
+
 #define MAX_LINE_LENGTH 80
 
- /*moves the index to the next place in string where the char isn't white */
-
-#define NOT_WHITE_SPACES(string,index) \
-for(;string[(index)] && (string[(index)] == "\t" || string[(index)] == ' '); (++(index)));
 
 
 
-int foundCommandSentence(char* file_name);
+
+
+int foundCommendSentence(char* file_name);
+
 int foundEmptySentence(char* file_name);
+
+int firstCharIsDot(char *line);
+
+int doCommandSentence(char *line, int *IC);
+
+char* cutColonFromLabel(char *line, char *firstWord);
+
+int doData(symbolTable table,char *line, int *DC,int numberOfLine);
+
+int doString(symbolTable table,char *line, int *DC,int numberOfLine);
+
+int doStruct(symbolTable table,char *line, int *DC,int numberOfLine);
+
+int doEntry(symbolTable table,char *line, int *DC,int numberOfLine);
+
+int doExtern(symbolTable table,char *line, int *DC,int numberOfLine);
+
+int crateSymbolTable(char* fileName);
+
 
 #endif
 
