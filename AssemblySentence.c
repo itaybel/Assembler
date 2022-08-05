f174e66fa7ce776598a363ddb78d4dbf6d88f2bf
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-08-05 13:33:38 +0300

    Update AssemblySentence.c

--- AssemblySentence.c
+++ AssemblySentence.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
 #include "AssemblySentence.h"
 #include "FileHandler.h"
@@ -9,10 +8,9 @@
 #include "SymbolTable.h"
 
 
-
-
 int foundEmptySentence(char* line){
-  
+
+
     if(line == NULL){
         return 1;
     }
@@ -41,28 +39,25 @@ int firstCharIsDot(char *line){
 }
 
 
-char* cutColonFromLabel(char *line, char *firstWord) {/*cuts from the label the ":" from the end*/
+
+char* cutColonFromLabel(char *line, char *firstWord) {
 
     line[strlen(firstWord) - 1] = '\0';
     return line;
 }
 
 
-int doData(symbolTable table,char *line, int *DC,int numberOfLine)
-{
+int doData(symbolTable table,char *line, int *DC,int numberOfLine,symbolTable symbol) {
     int i = 0;
     char *curr_line = line;
-    /*long ret;*/
-    while (curr_line[0] != '\n' && curr_line[0] != '\0')
-    {
-        /*ret = strtol(line, &curr_line, 10);*/
-        if (strcmp(curr_line, line) == 0)
-        { // this means that it couldn't convert any number
+
+
+    while (curr_line[0] != '\n' && curr_line[0] != '\0') {
+        if (strcmp(curr_line, line) == 0) { // this means that it couldn't convert any number
             printf("Couldn't parse one of the numbers!\n-%s-", curr_line);
             return 1;
         }
-        if (curr_line[0] != '\0' && curr_line[0] != ',')
-        {
+        if (curr_line[0] != '\0' && curr_line[0] != ',') {
             printf("Couldn't find an ',' after a number!");
             return 1;
         }
@@ -71,11 +66,14 @@ int doData(symbolTable table,char *line, int *DC,int numberOfLine)
         i++;
     }
     *DC = *DC + i;
+    if (symbol != NULL) {
+    setType(symbol, DATA_SYMBOL);
+    }
 
     return 0;
 }
 
-int doString(symbolTable table,char *line, int *DC,int numberOfLine)
+int doString(symbolTable table,char *line, int *DC,int numberOfLine,symbolTable symbol)
 {
     int i = 0;
     int string_length = 0;
@@ -105,13 +103,16 @@ int doString(symbolTable table,char *line, int *DC,int numberOfLine)
     if (found_valid_string)
     {
         *DC = *DC + string_length;
+        if (symbol != NULL) {
+            setType(symbol, DATA_SYMBOL);
+        }
         return 0;
     }
     return 1;
 }
 
 
-int doStruct(symbolTable table,char *line, int *DC,int numberOfLine)
+int doStruct(symbolTable table,char *line, int *DC,int numberOfLine,symbolTable symbol)
 {
     int i = 0;
     /*int ret = 0;*/
@@ -157,148 +158,159 @@ int doStruct(symbolTable table,char *line, int *DC,int numberOfLine)
     if (found_valid_string)
     {
         *DC = *DC + string_length + 1;
+        if (symbol != NULL) {
+            setType(symbol, STRUCT_SYMBOL);
+        }
         return 0;
     }
     return 1;
 }
 
 
-int doEntry(symbolTable table,char *line, int *DC,int numberOfLine){/*only checks if the label is valid*/
-    if(validLabelName(line)){
+int doEntry(symbolTable table,char *label, int *DC,int numberOfLine,symbolTable symbol){
+    if(validLabelName(label)){
         return 1;
     }
     return 0;
 }
 
 
-int doExtern(symbolTable table,char *line, int *DC,int numberOfLine){/*checks if the label is valid*/
-    if(validLabelName(line)){
+int doExtern(symbolTable table,char *label, int *DC,int numberOfLine, symbolTable symbol){
+
+    if(validLabelName(label)){
+        createSymbol(label,&DC);
+        InsertSymbolNode(symbol,&DC);
         return 1;
     }
 }
 
 
+void iCCounter(addressingMode address,addressingMode prevAddress, int *IC){
+
+    if (address == immediateAddress) {
+        (*IC)++;
+    }
+
+    if (address == directAddress) {/*firstWord = x: .data 23*/
+        (*IC)++;
+    }
+
+    if(address == directRegisterAddress && prevAddress != directRegisterAddress){
+        (*IC)++;
+
+    }
+
+    if (address== addressAccess) {/*firstWord = add #4, s.1,*/
+        (*IC) += 2;
+    }
 
 
-int doCommandSentence(char *line, int *IC) {
-    char *token = NULL;
+}
+
+
+
+
+int doCommandSentence(char *subString, int *IC,int numberOfLine,symbolTable symbol) {
     int opNumber = 0;
-    char *temp;
+    int i = 0;
+    char *temp = NULL;
+    addressingMode prevOperand = immediateAddress;
+    addressingMode curr = immediateAddress;
 
+    if (getOperationName(subString) == NULL) {
+        printf("Invalid OperationName");
+        return 0;
 
-    token = strtok(line, " \t\n\v\f\r");
-    opNumber = getOperandNum(token);
-
-    while (token != NULL)
-        token = strtok(NULL, ", ");
-    if (getAddressingMode(token) == immediateAddress) {/* token = mov #-1,r2*/
-        IC++;
     }
 
-    if (getAddressingMode(token) == directAddress) {/*token = x: .data 23*/
-        IC++;
-    }
+    opNumber = getOperandNum(subString);
 
-    if (getAddressingMode(token) == directRegisterAddress) {/*token = mov r1,r2*/
-        strcpy(temp, &token[2]);/*copy substring to check if substring is register as well*/
-        if (getRegisterName(temp)) {
-            IC++;
-        } else {
-            IC += 2;
-        }
-    }
 
-    if (getAddressingMode(token) == addressAccess) {/*token = add #4, s.1*/
-        IC += 2;
+    for (i = 0; i < opNumber; i++) {
+        subString = strtok(NULL, ", ");
+        if(subString < 2)
+        curr = getAddressingMode(subString);
+        iCCounter(curr, prevOperand, IC);
+        prevOperand = curr;
+
     }
 
     return 1;
-
 }
 
 
 struct inst
 {
     const char *name;
-    int(*doInstructions)(/*SymbolTable *, char */symbolTable table,char *line, int DC, int numberOfLine);
-}
+    int(*doInstructions)(/*SymbolTable *, char */symbolTable table,char *line, int *DC, int numberOfLine,symbolTable symbol);
+};
 
-validInstructions(char *instruction)
+void validInstructions(symbolTable table,char *instruction,int *DC, int numberOfLine,symbolTable symbol)
 {
     struct inst instructionFunc[] = {{".data", doData}, {".string", doString}, {".struct", doStruct}, {".entry",doEntry}, {".extern",doExtern}};
     int i = 0;
 
+
+
     for(i = 0; i < sizeof(instructionFunc)/sizeof(instructionFunc[0]); ++i)
     {
         if(strcmp(instructionFunc[i].name, instruction) == 0){
-            instructionFunc[i].doInstructions(Symbol, line, DC, numberOfLine);
+            instruction = strtok(NULL," ");
+            instructionFunc[i].doInstructions(table, instruction, DC, numberOfLine,symbol);
         }
+        
     }
 }
 
 
 
-int crateSymbolTable(char* fileName){
+
+int crateSymbolTable(char* fileName) {
 
     FILE *inputFile = NULL;
     char line[MAX_LINE_LENGTH] = {0};
     char *originalLine = NULL;
     char *firstWord = NULL;
-    char *token = NULL;
     char *label = NULL;
+    symbolTable symbol = NULL;
+    symbolTable table = NULL;
+    int numberOfLine = 0;
     int i = 0;
 
-    static int IC,DC;
+    int IC = 100;
+    int DC = 0;
 
-    inputFile = openFile(fileName,"am","r");
+    inputFile = openFile(fileName, "am", "r");
 
-    /* iterating through each line of the input file */
-    fgets(line, MAX_LINE_LENGTH, inputFile); /* XYZ: .data 7, -57, +17, 9*/
-    /* storing a copy of the line, since strtok will be changing it */
-    strcpy(originalLine, line);
-    /* token recives the first string*/
-    firstWord = strtok(originalLine, " \t\n\v\f\r");/*XYZ:*/
 
-        while (!feof(inputFile)) {
+    while (!feof(inputFile)) {
 
-            if(foundEmptySentence(originalLine) || foundCommendSentence(originalLine)){/* if line is empty or commend continue to the next line*/
-                continue;
-            }
+        /* iterating through each line of the input file */
+        fgets(line, MAX_LINE_LENGTH, inputFile); /* XYZ: .data 7, -57, +17, 9*/
+        numberOfLine++;
+        /* storing a copy of the line, since strtok will be changing it */
+        strcpy(originalLine, line);
+        /* subString recives the first string*/
+        firstWord = strtok(originalLine, " \t\n\v\f\r");/*XYZ:*/
 
-            if(validLabelName(firstWord)) {/* if firstWord is label XYZ: we cuting the colon(:) from it,*/
-                label = cutColonFromLabel(originalLine,firstWord);
-            }
 
-            else if(getInstructionName(firstWord)) {
-                validInstructions(firstWord);/*need to check all the type of sentence, and the function need to return the do instruction that we found in the line*/
-                if(symbolHead((symbolTable *) label, DC) == NULL) {
-                    crateTable(label, DC,DC);/*if we had label before instruction we crate the symbolTable for first time*/
-                    symbolHead((symbolTable *) label, DC);
-                } else{/*if symbolTable is not empty we only add new symbol to the table*/
-                    symbolHead((symbolTable *) label, DC);
+        if (foundEmptySentence(originalLine) ||
+            foundCommendSentence(originalLine)) {/* if line is empty or commend continue to the next line*/
+            continue;
+        }
 
-                }
+        if (isLabel(firstWord)) {/* if subString is label XYZ: we cuting the colon(:) from it,*/
+            label = cutColonFromLabel(originalLine, firstWord);
+            createSymbol(label, IC);
+            firstWord = strtok(NULL, "");
+        }
 
-            } else{/*if the firstword in line is not instruction its command*/
-                doCommandSentence(originalLine,  IC);
-                if(symbolHead((symbolTable *) label, DC) == NULL) {
-                    crateTable(label, DC,DC);/*if we had label before instruction we crate the symbolTable for first time*/
-                    symbolHead((symbolTable *) label, DC);
-                } else{/*if symbolTable is not empty we only add new symbol to the table*/
-                    symbolHead((symbolTable *) label, DC);
-            }
-
-            /* iterating through each line of the input file */
-            fgets(line, MAX_LINE_LENGTH, inputFile); /*XYZ: .data 7, -57, +17, 9*/
-
-            /* storing a copy of the line, since strtok will be changing it */
-            strcpy(originalLine, line);
-            /* token receives the next char in line*/
-            token = strtok(NULL," ,");
-
-            }
+        if (firstCharIsDot(firstWord)) {
+            validInstructions(table, firstWord, &DC, numberOfLine, symbol);
+        } else {
+            doCommandSentence(firstWord, &IC, numberOfLine, symbol);
         }
 
 
-
-
+    }
+}
