029e50bfda95ef143b30b2c2dbf1208bee92f013
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-08-05 13:27:13 +0300

    Update SymbolTable.c

--- SymbolTable.c
+++ SymbolTable.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <ctype.h>
 #include "SymbolTable.h"
 #include "AddressingMode.h"
 
 
 
-/*typedef struct symbolNode
-{
-    char* name;
+/** A single table symbol*/
+struct symbol {
+    /** Next symbol in table */
+    symbolTable next;
+    /** Address of the symbol */
     int address;
-    char* def;
+    /** Key (symbol name) is a string (aka char*) */
+    char *key;
+    /** Symbol type */
+    symbolType type;
+};
 
 
-*//* symbol can be external,can be entry, .data, or command*//*
-} SymbolNode;*/
+int isLabel(char *name){
+    if(validLabelName(name) && name[strlen(name) -1] == ':'){
+        return 1;
+    }
+    return 0;
+}
 
 
-static int IC,DC,L;
-
-
+/*Check length, first char is alpha and all the others are alphanumeric, and not assembly reserved identifier and string size is 30*/
 int validLabelName(char *name) {
-    /*Check length, first char is alpha and all the others are alphanumeric, and not assembly reserved identifier and string size is 30*/
+
     int i = 0;
 
-    if (name[0] && strlen(name) <= MAX_LENGTH && isalpha(name[0]) && reservedWord(name) && name[strlen(name) -1] == ':') {
+    if (name[0] && strlen(name) <= MAX_LENGTH && isalpha(name[0]) && !(reservedWord(name))){
         while (name[i]) {/*use loop to check isalnum if the word is alphanumeric*/
             if (isalnum(name[i])) {
                 i++;
@@ -52,40 +59,92 @@ void *checkMalloc(int size) {
 
 
 
-void crateTable(char *key, int symbolAddress, symbolType type) {
-    char *tempKey;
-    symbolTable newSymbol;
-    /* allocate memory for new symbol */
-    newSymbol = (symbolTable) checkMalloc(sizeof(singleSymbol));
-    /* Prevent "Aliasing" of pointers. when we free the list, we also free these allocated char ptrs */
-    tempKey = (char *) checkMalloc(strlen(key) + 1);
-    strcpy(tempKey, key);
-    newSymbol->key = tempKey;
-    newSymbol->address = symbolAddress;
-    newSymbol->type = type;
+
+
+void setType(symbolTable symbol,symbolType type){
+
+    symbol->type = type;
 }
 
-void symbolHead(symbolTable *sym, int address){
 
-    symbolTable prevSymbol, currSymbol, newSymbol = NULL;
-    /* if the table's null, set the new symbol as the head. */
-    if ((*sym) == NULL || (*sym)->address > address) {
-        newSymbol->next = (*sym);
-        (*sym) = newSymbol;
-        return;
-    }
+void setAddress(symbolTable symbol, int address){
 
-    /* Insert the new symbol table, keeping it sorted */
-    currSymbol = (*sym)->next;
-    prevSymbol = *sym;
-    while (currSymbol != NULL && currSymbol->address < address) {
-        prevSymbol = currSymbol;
-        currSymbol = currSymbol->next;
-    }
-
-    newSymbol->next = currSymbol;
-    prevSymbol->next = newSymbol;
+    symbol->address = address;
 }
+
+
+
+int compareSymbol(symbolTable symbol, char *key){
+    return strcmp((symbol)->key, key);
+}
+
+
+
+void InsertSymbolNode(symbolTable head_ref, void* new_data)
+{
+
+/* Allocate node*/
+
+    symbolTable new_node = createSymbol(head_ref,new_data);
+
+/*put in the data*/
+
+
+    new_node->address  = malloc(sizeof(new_data));
+
+    new_node->address  = new_data;
+    if (!(head_ref))
+
+/* if the list is empty */
+
+    {
+        head_ref = new_node;
+
+    }else{
+
+/*Make next of new node as head*/
+
+        new_node->next = (head_ref);
+
+
+/*move the head to point to the new node*/
+
+        (head_ref)  = new_node;
+    }
+}
+
+
+int getAddress(symbolTable symbol){
+    return symbol->address;
+}
+
+
+symbolType getType(symbolTable symbol){
+    return symbol->type;
+}
+
+
+char *getSymbol(symbolTable symbol){
+    return symbol->key;
+}
+
+
+
+
+symbolTable createSymbol(char* key,int address){
+
+    symbolTable new_node = malloc(sizeof(struct symbol));
+    if(new_node == NULL){
+        printf("Error, couldn't allocate memory");
+        return  NULL;
+    }
+    new_node -> key = (char*)malloc((strlen(key) + 1) * sizeof(char));
+    strcpy(new_node->key, key);
+
+    return new_node;
+
+}
+
 
 void freeTable(symbolTable table) {
     symbolTable prevSymbol, currSymbol = table;
@@ -95,55 +154,5 @@ void freeTable(symbolTable table) {
         free(prevSymbol->key);
         free(prevSymbol);
     }
-}
-
-void addValueToType(symbolTable tab, int addAddress, symbolType type) {
-    symbolTable currSymbol;
-    /* for each entry, add address to_add if same type */
-    for (currSymbol = tab; currSymbol != NULL; currSymbol = currSymbol->next) {
-        if (currSymbol->type == type) {
-            currSymbol->address += addAddress;
-        }
-    }
-}
-
-symbolTable filterTableByType(symbolTable table, symbolType type) {
-    symbolTable newTable = NULL;
-    /* For each symbol, check if he has the type. if so, insert to the new table. */
-    do {
-        if (table->type == type) {
-            symbolHead(&newTable, table->address);
-        }
-    } while ((table = table->next) != NULL);
-    return newTable; /* It holds a pointer to the first symbol, dynamically-allocated, so it's fine (not in stack) */
-}
-
-singleSymbol *findByTypes(symbolTable table, char *key, int symbolCount, ...) {
-    int i;
-    symbolType *validSymbolTypes = checkMalloc((symbolCount) * sizeof(int));
-    /* Build a list of the valid types */
-    va_list arglist;
-    va_start(arglist, symbolCount);
-    for (i = 0; i < symbolCount; i++) {
-        validSymbolTypes[i] = va_arg(arglist, symbolType);
-    }
-    va_end(arglist);
-    /* table null => nothing to dos */
-    if (table == NULL) {
-        free(validSymbolTypes);
-        return NULL;
-    }
-    /* iterate over table and then over array of valid. if type is valid and same key, return the symbol. */
-    do {
-        for (i = 0; i < symbolCount; i++) {
-            if (validSymbolTypes[i] == table->type && strcmp(key, table->key) == 0) {
-                free(validSymbolTypes);
-                return table;
-            }
-        }
-    } while ((table = table->next) != NULL);
-    /* not found, return NULL */
-    free(validSymbolTypes);
-    return NULL;
 }
 
