e87b0ef89608d0b9d227cc6d3cdb16f2caf086b1
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-08-05 13:29:19 +0300

    Update SymbolTable.h

--- SymbolTable.h
+++ SymbolTable.h
@@ -18,43 +18,39 @@ void *checkMalloc(int size);
 
 /** A symbol type */
 typedef enum symbolType {
-    CODE_SYMBOL,
-    DATA_SYMBOL,
-    EXTERNAL_SYMBOL,
-    STRUCT_SYMBOL
+    CODE_SYMBOL,/* opertion_names and ect*/
+    DATA_SYMBOL,/*string
+    EXTERNAL_SYMBOL, /*when we have instruction on extern*/
+    STRUCT_SYMBOL /*when we have instruction on struct*/
 } symbolType;
 
 /** pointer to table symbol is just a table. */
 typedef struct symbol* symbolTable;
 
-/** A single table symbol*/
-typedef struct symbol {
-    /** Next symbol in table */
-    symbolTable next;
-    /** Address of the symbol */
-    int address;
-    /** Key (symbol name) is a string (aka char*) */
-    char *key;
-    /** Symbol type */
-    symbolType type;
-} singleSymbol;
 
-/**
- * Adds an item to the table, keeping it sorted.
- * @param sym A pointer to the symbolTable
- * @param key The key of the symbol to insert
- * @param symbolAddress The address of the symbol to insert
- * @param type The type of the symbol to insert
- */
-void crateTable(char *key, int symbolAddress, symbolType type);
+int isLabel(char *name);
 
 
-/**
- * Adds symbol to head of the table.
- * @param sym The symbol
- * @param symbolAddress The address of the symbol
- */
-void symbolHead(symbolTable *sym, int address);
+
+
+void setType(symbolTable symbol,symbolType type);
+
+
+int compareSymbol(symbolTable symbol, char *key);
+
+void addType(symbolTable symbol, symbolType type);
+
+void setAddress(symbolTable symbol, int address);
+
+void InsertSymbolNode(symbolTable head_ref, void* new_data);
+
+int getAddress(symbolTable symbol);
+
+symbolType getType(symbolTable symbol);
+
+char *getSymbol(symbolTable symbol);
+
+symbolTable createSymbol(char* key,int address);
 
 
 /**
@@ -64,31 +60,5 @@ void symbolHead(symbolTable *sym, int address);
 void freeTable(symbolTable table);
 
 
-/**
- * Adds the address of each symbol
- * @param tableAddress The table, containing the entries
- * @param addAddress The address to add
- * @param type The type of symbols to add the address to
- */
-void addValueToType(symbolTable tableAddress, int addAddress, symbolType type);
-
-/**
- * Returns all the symbols by their type in a new table
- * @param table The table
- * @param type The type to look for
- * @return A new table, which contains the symbols
- */
-symbolTable filterTableByType(symbolTable table, symbolType type);
-
-/**
- * Find symbol from the only specified types
- * @param table The table
- * @param key The key of the symbol
- * @param symbolCount The count of given types
- * @param ... The types to filter
- * @return The entry if found, NULL if not found
- */
-singleSymbol *findByTypes(symbolTable table,char *key, int symbolCount, ...);
 
 #endif
-
