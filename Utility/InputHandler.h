4104685efce002fb048703733df732ca84a00f06
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-08-10 19:01:32 +0300

    Update InputHandler.h

--- Utility/InputHandler.h
+++ Utility/InputHandler.h
@@ -6,16 +6,26 @@
 #include <stdlib.h>
 #include "string.h"
 #define MAX_FILE_NAME_LENGTH 255
-#define MAX_LINE_LENGTH 82
-#include "../PreAssembler/MacroTable.h"
+#include "MacroTable.h"
 
 
 
 void removeSpacesAndTabs(char line[MAX_LINE_LENGTH]);
 
+void throwError(char* errorMsg, int numberOfLine);
 
 int containsOnlyBlanks(char line[MAX_LINE_LENGTH]);
 
 void substring(char* string, int length);
+
+getSpacesAtBegining(char* substring);
+
+int firstCharIsDot(char *line);
+
+char* cutColonFromLabel(char *line, char *firstWord);
+
+int isNumber(char *number);
+
+
 
 #endif
