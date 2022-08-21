4560ea73e34adac5fb84cf83f7a94fe887f1f647
Max Freeman <69054596+MaxFreeman-enterprise@users.noreply.github.com>
2022-08-21 18:27:08 +0300

    Update AddressingMode.h
    fixed includes and remarkes typo

--- AddressingMode.h
+++ AddressingMode.h
@@ -1,17 +1,8 @@
 #ifndef ADDRESSINGMODE_H
 
 #define ADDRESSINGMODE_H
-#include "RegisterTable.h"
-#include "OperationTable.h"
-
-
 
 #define NUM_OF_ADDRESS_MODE 5
-
-
- /*Instruction type (.data, .entry, etc.)*/
-
-
 
 typedef enum addressingMode{
     immediateAddress = 0,
@@ -24,10 +15,9 @@ typedef enum addressingMode{
 /*
 This function checks if a given string is an instruction name
 @param name - the string to be checked
-@return wether its a valid instruction (.data/.string/.struct/.entry/.extern)
+@return weather it's a valid instruction (.data/.string/.struct/.entry/.extern)
 */
 int isInstructionName(char *name);
-
 
 
 /*
@@ -42,7 +32,7 @@ int getFirstDelimIndex(char *str, char delim);
 /*
 This function returns the addressing mode of a given operand
 @param operand - the operand to get its addressing mode
-@param numberOfLine - the current line proccesed in the input file
+@param numberOfLine - the current line processed in the input file
 @return the addressing mode of the given operand
 */
 enum addressingMode getAddressingMode(char *operand, int numberOfLine);
